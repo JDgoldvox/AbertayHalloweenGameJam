4fcf51d3afdb770b2e4a177a3afe2b442c8f2964
JDgoldvox <voxcastJDgoldvox@gmail.com>
2024-11-02 16:17:36 +0000

    jump created

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform camTransform;
     private Rigidbody rb;
-    private float speedMultiplier = 1.5f;
+    private float speedMultiplier = 0.6f;
+    [SerializeField] float jumpMultiplier;
+    float jumpTimer = 0;
+    float jumpCooldown = 1;
 
-    [SerializeField] private float linearVelocityCap;
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,32 +24,59 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         UpdateMovement();
+        Jump();
     }
 
     private void UpdateMovement()
     {
+        bool isMoving = false;
         Vector3 direction = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W)) //Forward
         {
             direction += camTransform.forward;
+            isMoving = true;
         }
 
         if (Input.GetKey(KeyCode.A)) //Right
         {
             direction += -camTransform.right;
+            isMoving = true;
         }
 
         if (Input.GetKey(KeyCode.D)) //Left
         {
             direction += camTransform.right;
+            isMoving = true;
         }
 
         if (Input.GetKey(KeyCode.S)) //Backwards
         {
             direction += -camTransform.forward;
+            isMoving = true;
         }
 
-        rb.AddForce(direction.normalized * speedMultiplier, ForceMode.Impulse);
+        if (isMoving)
+        {
+            rb.AddForce(direction.normalized * speedMultiplier, ForceMode.Impulse);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && jumpTimer <= 0) //jump
+        {
+            Vector3 direction = Vector3.up + (camTransform.forward * 2);
+            direction = direction.normalized;
+            direction *= jumpMultiplier;
+
+            rb.AddForce(direction, ForceMode.Impulse);
+            jumpTimer = jumpCooldown;
+        }
+        else
+        {
+            //update timer
+            jumpTimer -= Time.deltaTime;
+        }
     }
 }
